d11702aa420224cc5e6b730efddf179d322dfa9e
Suraj Kr Singh <77935554+SurajSingh05054@users.noreply.github.com>
2023-11-23 10:54:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,9 @@
 
 let menu = document.querySelector("#menu-icon");
 let right = document.querySelector(".nright");
-let section = document.querySelector(".Section")
 
 menu.onclick =() => {
     menu.classList.toggle("bx-x");
     right.classList.toggle("open");
-    section.classList.toogle("call");
-    h1.classList.toggle("change");
     
 }
