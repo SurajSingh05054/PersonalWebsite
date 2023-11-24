60f3a24640d4d3d8035c78d31c0d8609d7fb3e78
Suraj Kr Singh <77935554+SurajSingh05054@users.noreply.github.com>
2023-11-24 13:48:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ menu.onclick =() => {
     right.classList.toggle("open");
     
 }
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "suvi6579@gmail.com",
+        Password : "67EDF7E978D626EE2971200AB054D0040784",
+        To : 'suvi6579@gmail.com',
+        From : document.querySelector("#email").value,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );  
+}
